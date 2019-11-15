0857f2947e1a5e5409018cdb8824c714cf837321
israel dantas <israeldantasleite@gmail.com>
2019-11-15 13:09:53 -0300

    change version image

--- api/index.js
+++ api/index.js
@@ -5,12 +5,12 @@ const fakerResponse = require('./faker')
 
 const monitor = require('express-status-monitor')
 
-app.use(monitor());
+app.use(monitor())
 
-app.get('/', function(req, res) {
+app.get('/', function(_, res) {
   res.status(200).json(fakerResponse);
-});
+})
 
 app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!');
-});
+  console.log('App de Exemplo escutando na porta 3000!')
+})
